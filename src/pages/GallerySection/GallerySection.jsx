febf758f4cb8293de4f913ae17a421f800f74223
jkjoy99 <jkjoy99@gmail.com>
2023-05-21 21:30:43 +0600

    footer updated

--- src/pages/GallerySection/GallerySection.jsx
+++ src/pages/GallerySection/GallerySection.jsx
@@ -7,8 +7,6 @@ const GallerySection = ({ toy }) => {
             <div className="card card-compact w-96 bg-base-100">
                 <figure className='w-full h-96 '><img src={toy.image} alt="Shoes" /></figure>
             </div>
-         
-
         </div>
     );
 };
